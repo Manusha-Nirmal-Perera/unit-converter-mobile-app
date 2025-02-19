62ef281548051838bdc139f9e3454a8ab8a4f73c
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-02-19 19:17:41 +0530

    Added the dropdown menues

--- app/src/main/java/com/company/unitconverter/MainActivity.kt
+++ app/src/main/java/com/company/unitconverter/MainActivity.kt
@@ -14,9 +14,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -45,7 +48,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
 
@@ -53,6 +58,7 @@ fun UnitConverter() {
         Text("Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "Enter a value", onValueChange = {} )
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
 //            val context = LocalContext.current
 //            Button(onClick = {
@@ -70,13 +76,49 @@ fun UnitConverter() {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("MilliMeters") },
+                        onClick = {}
+                    )
+                }
             }
 
-
+            Spacer(modifier = Modifier.width(16.dp ))
             Box {
                 Button(onClick = {}) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("MilliMeters") },
+                        onClick = {}
+                    )
                 }
             }
 
